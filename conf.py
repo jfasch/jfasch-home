b0195c4adccc4f00926e2610d5122126e51f8467
Joerg Faschingbauer <jf@faschingbauer.co.at>
2021-12-26 15:05:25 +0100

    switch to RTD theme

--- conf.py
+++ conf.py
@@ -7,14 +7,14 @@
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
+
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 
 import logging
 import os
-import sys
-#sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
+
 
 # -- Project information -----------------------------------------------------
 _me = 'Jörg Faschingbauer'
@@ -45,14 +45,34 @@ extensions = [
     'sphinxcontrib.images',
     'sphinxemoji.sphinxemoji',
 
+    'sphinx_rtd_theme',
     'opentraining',
 ]
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'collapse_navigation': False,
+    'navigation_depth': -1,
+    'prev_next_buttons_location': None,
+    
+    # 'includehidden': False,
+    # 'style_external_links': True,
+}
+
+# context that jinja (and rst, via the rstjinja extension) sees.
+html_context = {
+    # experimental: this is how we can get settings into templates
+    'READTHEDOCS': False, # {en,dis}able versions.html
+    'versions': [('blah', 'https://www.google.com'),],
+}
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'opentraining': ('https://opentraining.readthedocs.io/en/latest/', None),
     'pytest': ('https://docs.pytest.org/en/latest/', None),
     'numpy': ('https://numpy.org/doc/stable/', None),
 }
+
 todo_include_todos = True
 
 # ablog
@@ -94,164 +114,6 @@ exclude_patterns = [
 html_context = {}
 html_static_path = ['_static']
 
-_BASIC = 'basic'
-_RTD = 'sphinx_rtd_theme'
-_BETTER = 'better'
-_CLOUD = 'cloud'
-_PYRAMID = 'pyramid'
-_PRESS = 'press'
-_NATURE = 'nature'
-_ALABASTER = 'alabaster'
-_BOOTSTRAP = 'bootstrap'
-
-html_theme = _ALABASTER
-html_theme_path = []
-html_theme_options = {}
-
-_jf_csss = []
-
-if html_theme == _ALABASTER:
-    # RANT: the settings below (page_width 90%, sidebar_width 20% (of
-    # page_width, essentially)) result in 
-
-    # * div.bodywrapper: margin-left: 20% (=274.75 on my screen)
-
-    # * div.sphinxsidebar: width: 20% (305.267)
-
-    # though the plan is obviously that they have the same calculated
-    # size, so the sphinxsidebar fits in bodywrapper's left
-    # margin. (sidenote: why the f**k is it done in this way? why not
-    # just position clearly with ... whatever ... some flexbox fancy?)
-
-    # however: this *not* the case. for some reason (*), the
-    # calculated values are *not* equal, and the sidebar's
-    # width=305.267 overlaps into bodywrapper's 274.75 margin-left.
-
-    # (*) hmm ... when I look at the element hierarchy, I see that
-
-    # * div.sphinxsidebar is a child of div.document
-
-    # * div.bodywrapper is a child of div.documentwrapper which is a
-    #   child of div.document
-
-    # question: how can it be that bodywrapper'div.s margin-left
-    # calculates to a value that appears to be derived from
-    # div.document?
-
-    # ANYWAY: stick with it, but keep looking out for better
-    # solutions/themes. learn to do web crap better.
-
-    import alabaster
-
-    html_theme_path.append(alabaster.get_path())
-
-    templates_path.append('_templates')
-
-    html_theme_options.update({
-        'show_powered_by': False,
-        'fixed_sidebar': True,
-        'logo': 'logo.png',
-        'description': 'Linux und Open Source',
-        'page_width': '90%',
-        'sidebar_width': '20%',
-
-        # hardwired by default by "basic" theme, to 800px. no idea
-        # why. occupy what we can get.
-        'body_max_width': '100%',
-
-        # 'github_banner': True,
-        # 'github_button': True,
-
-        'font_family': '"Lucida Sans Unicode", "Lucida Grande", sans-serif;'
-    })
-
-    html_sidebars = {
-        # the more specific match must come first (fortunately
-        # dictionary order is preserved since 3.7 :-) prior to that we
-        # would have had no chance to fix that other than changing
-        # html_sidebars to a list of tuples).
-        'blog/**': [
-            'about.html',
-            'searchbox.html',
-            'navigation.html',
-            'postcard.html',
-        ],
-        '**': [
-            'about.html',
-            'searchbox.html',
-            'navigation.html',
-        ],
-    }
-
-    html_context.update({
-        'jf_nav_bg': '#f0f3f8',
-        'jf_info_bg': '#fcfcfc',
-        'jf_note_bg': '#eee',
-    })
-
-    _jf_csss.append('css/jf-alabaster.css')
-
-if html_theme == _RTD:
-    # nav and location feedback really great
-    
-    # looks good on mobile
-    
-    # on wider screen a lot of screen real estate to the right is wasted
-    
-    # html_sidebars disregarded
-
-    # RTD css unreadable (uglified?)
-    
-    extensions.append('sphinx_rtd_theme')
-
-    html_theme_options['collapse_navigation'] = False
-    html_theme_options['navigation_depth'] = -1
-    
-    # context that jinja (and rst, via the rstjinja extension) sees.
-    html_context.update({
-        # experimental: this is how we can get settings into templates
-        'READTHEDOCS': False, # {en,dis}able versions.html
-        'versions': [('blah', 'https://www.google.com'),],
-    
-        # reading the RTD theme sources, I see that this is the RTD way of
-        # adding my own css.
-        'extra_css_files': ['_static/css/jf.css'],
-    })
-
-if html_theme == _BETTER:
-    from better import better_theme_path
-    html_theme_path = [better_theme_path]
-
-if html_theme == _CLOUD:
-    html_theme_options['max_width'] = '100%'
-    pass
-
-if html_theme == _PYRAMID:
-    import pyramid_sphinx_themes
-    html_theme_path += pyramid_sphinx_themes.get_html_themes_path()
-
-if html_theme == _PRESS:
-    # html_sidebars as set above won't work. emptying it does,
-    # somehow.
-
-    # nav is buggy.
-    pass
-
-if html_theme == _NATURE:
-    pass
-
-if html_theme == _BASIC:
-    pass
-
-if html_theme == _BOOTSTRAP:
-    # just a try, no navigation with location feedback
-
-    import sphinx_bootstrap_theme
-    html_theme_path += sphinx_bootstrap_theme.get_html_theme_path()
-
-
-
-
 def rstjinja(app, docname, source):
     """Render our pages as a jinja template for fancy templating goodness.
 
@@ -268,10 +130,9 @@ def rstjinja(app, docname, source):
     source[0] = rendered
 
 def setup(app):
-    for css in _jf_csss:
-        app.add_css_file(css)
-
     app.connect("source-read", rstjinja)
+
+    app.add_css_file('css/jf.css')
 
     # html_sidebars has two matches, and both match any /blog/
     # docname. the first match is more specific than the second, which
