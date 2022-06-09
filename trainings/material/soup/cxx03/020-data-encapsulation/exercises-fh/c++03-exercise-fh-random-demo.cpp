cf4b6e538e4e18a7b5552c86370fa88c9cac745d
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-06-09 13:08:25 +0200

    fh: random-sensor exercise

---
+++ trainings/material/soup/cxx03/020-data-encapsulation/exercises-fh/c++03-exercise-fh-random-demo.cpp
@@ -0,0 +1,23 @@
+#include <random>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    if (argc != 3) {
+        std::cerr << argv[0] << ": <low> <high>" << std::endl;
+        return 1;
+    }
+
+    double low = std::stod(argv[1]);
+    double high = std::stod(argv[2]);
+
+    std::uniform_real_distribution<double> distribution(low, high);  // <--- HERE
+    std::default_random_engine engine;                               // <--- HERE
+
+    while (true) {
+        double number = distribution(engine);                        // <--- HERE
+        std::cout << number << std::endl;
+    }
+
+    return 0;
+}
