56e78b544134aef4310d7bb5ee51f9ea274a5022
Joerg Faschingbauer <jf@faschingbauer.co.at>
2023-04-13 12:42:03 +0100

    blink

--- about/site/work-in-progress/blink/code/thread-multi.py
+++ about/site/work-in-progress/blink/code/thread-multi.py
@@ -4,19 +4,19 @@ from threading import Thread
 import time
 
 def hello_left():
-    for _ in range(5):
+    for _ in range(10):
         print('hello left')
         time.sleep(0.5)
 
 def hello_right():
-    for _ in range(5):
+    for _ in range(10):
         print('hello right'.rjust(60))
-        time.sleep(0.8)
+        time.sleep(0.4)
 
 def hello_middle():
-    for _ in range(4):
+    for _ in range(10):
         print('hello middle'.center(60))
-        time.sleep(1)
+        time.sleep(0.3)
 
 t1 = Thread(target=hello_left)
 t2 = Thread(target=hello_right)
