d9df511eeb1844b23dcb64b680ea30cf7b29e71c
Joerg Faschingbauer <jf@faschingbauer.co.at>
2025-04-22 13:53:28 +0200

    glt2025: remove dgram stuff

--- about/site/work-in-progress/glt2025/02-raw-dgram-multiple-sockets-mt/send-dgram.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import socket, sys
-
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-
-while True:
-    msg = input('message (utf-8 please): ')
-    sock.sendto(msg.encode(encoding='utf-8'), sys.argv[1])
